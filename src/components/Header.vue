66d9980e3f03c754cc672b4a8ca4c8a7e8b440dd
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-07-16 14:03:52 +0200

    add header top

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,33 @@
 <template>
   <div>
     <!-- inizio header top -->
-    <div class="header-top d-flex justify-content-between">
-      <div class="left">
-        (001) 88451234
+    <div class="header-top text-white">
+      <!-- inizio contenitore -->
+      <div class="container d-flex justify-content-between align-items-center">
+
+        <!-- inizio parte sx -->
+        <div class="left">
+          <i class="fas fa-phone-alt"></i>
+          <span>(001) 88451234</span>
+        </div>
+        <!-- fine parte sx -->
+
+        <!-- inizio parte dx -->
+        <div class="right">
+          <i class="fab fa-facebook-f"></i>
+          <i class="fab fa-twitter"></i>
+          <i class="fab fa-pinterest-p"></i>
+        </div>
+        <!-- fine parte dx -->
+
       </div>
+      <!-- fine contenitore -->
     </div>
     <!-- fine header top -->
 
     <!-- inizio header bottom -->
     <div class="header-bottom">
-      
+
     </div>
     <!-- fine header bottom -->
   </div>
@@ -27,5 +44,14 @@ export default {
 <style lang="scss">
   .header-top{
     background-color: #3d3d3d;
+
+    .container{
+      height: 50px;
+
+      .left > *,
+      .right > *{
+        margin: 0 10px;
+      }
+    }
   }
 </style>
