068774ec7a7d35f5ffaa0c91e5d008231750cc7b
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-07-17 18:19:19 +0200

    add footer top

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,44 @@
 
     <!-- inizio footer top -->
     <div class="container-fluid">
-      <div class="top">
+      <div class="top row row-cols-4 my-5 py-5">
+
+        <!-- inizio colonna 1 -->
+        <div>
+          <img src="@/assets/img/logo_big.png" alt="logo phlox" class="mb-3">
+          <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse architecto suscipit incidunt nam hic quos rerum voluptatibus eum dignissimos et, accusantium cupiditate, repellendus ex corporis. Omnis voluptatem totam explicabo excepturi.</div>
+        </div>
+        <!-- fine colonna 1 -->
+
+        <!-- inzio colonna 2 -->
+        <div>
+          <div class="title">Quick links</div>
+          <div class="divider my-3"></div>
+          <ul>
+            <li v-for="(link,i) in quickLinks" :key="i">
+              <a :href="link.link"> {{link.text}} </a>
+            </li>
+          </ul>
+        </div>
+        <!-- fine colonna 2 -->
+
+        <!-- inizio colonna 3 -->
+        <div>
+          <div class="title">Contact</div>
+          <div class="divider my-3"></div>
+          <div>Patricia V. Amedee 4401 Waldeck Street Grepevine Nashville, TX 76051</div>
+          <div class="my-3">info@yourdomain.com</div>
+          <div>+99 (0) 101 0000 888</div>
+        </div>
+        <!-- fine colonna 3 -->
+
+        <!-- inizio colonna 4 -->
+        <div>
+          <div class="title">Location on map</div>
+          <div class="divider my-3"></div>
+          <img src="@/assets/img/map.png" alt="map img" class="w-100">
+        </div>
+        <!-- fine colonna 4 -->
 
       </div>
     </div>
@@ -28,13 +65,71 @@
 
 <script>
 export default {
-  name : 'Footer'
+  name : 'Footer',
+  data(){
+    return{
+      quickLinks : [
+        {
+          text : 'Careers',
+          link : '#'
+        },
+        {
+          text : 'News',
+          link : '#'
+        },
+        {
+          text : 'Terms of use',
+          link : '#'
+        },
+        {
+          text : 'Privacy Project',
+          link : '#'
+        },
+        {
+          text : 'About',
+          link : '#'
+        },
+        {
+          text : 'Contact',
+          link : '#'
+        }
+      ]
+    }
+  }
 }
 </script>
 
 
 <style lang="scss">
   .footer{
+
+    .top{
+
+      & > *{
+        padding: 2rem;
+      }
+
+      .title{
+        text-transform: uppercase;
+        font-size: 1.5rem;
+        font-weight: bold;
+      }
+
+      ul{
+        list-style: none;
+        padding: 0;
+
+        li a{
+          text-decoration: none;
+          color: black;
+
+          &:hover{
+            color: #00d9a6;
+            cursor: pointer;
+          }
+        }
+      }
+    }
 
     .bottom{
       height: 100px;
