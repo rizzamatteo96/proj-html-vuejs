6f4b7ba50df5e4a57a44eb41188f0bd5b72039cf
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-07-17 18:00:09 +0200

    add footer bottom

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,27 @@
 <template>
-  <div>
-    
+  <div class="footer">
+
+    <!-- inizio footer top -->
+    <div class="container-fluid">
+      <div class="top">
+
+      </div>
+    </div>
+    <!-- fine footer top -->
+
+    <!-- inizio footer bottom -->
+    <div class="bottom d-flex justify-content-between align-items-center text-white px-5">
+      <div class="left text-uppercase">
+        &copy; 2020 Phlox business theme.
+      </div>
+      <div class="right">
+        <i class="fab fa-facebook-f"></i>
+        <i class="fab fa-twitter"></i>
+        <i class="fab fa-pinterest-p"></i>
+      </div>
+    </div>
+    <!-- fine footer bottom -->
+
   </div>
 </template>
 
@@ -13,5 +34,15 @@ export default {
 
 
 <style lang="scss">
+  .footer{
 
+    .bottom{
+      height: 100px;
+      background-image: linear-gradient(to right, rgb(54, 54, 54) , black);
+
+      .right > *{
+        margin: 0 10px;
+      }
+    }
+  }
 </style>
