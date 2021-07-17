833865c96d6b4aa39dea286d42424d4edf7a06a8
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-07-17 15:28:24 +0200

    add main section 2

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -7,6 +7,9 @@
     <!-- Caricamento prima sezione -->
     <Section1/>
 
+    <!-- Caricamento della seconda sezione -->
+    <Section2/>
+
   </main>
 </template>
 
@@ -14,12 +17,14 @@
 <script>
 import Slider from './Slider.vue'
 import Section1 from './main_sections/Section1.vue'
+import Section2 from './main_sections/Section2.vue'
 
 export default {
   name : 'Main',
   components : {
     Slider,
-    Section1
+    Section1,
+    Section2,
   }
 }
 </script>
---
+++ src/components/main_sections/Section2.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="section2">
+    <div class="container-fluid my-5">
+      <div class="row row-cols-2 g-0 align-items-center">
+        <!-- img + text -->
+        <!-- inizio parte sinistra -->
+        <div class="left pe-5">
+          <img class="w-100 p-3" src="@/assets/img/business-woman.jpg" alt="business woman">
+        </div>
+        <!-- fine parte sinistra -->
+
+        <!-- inizio parte destra -->
+        <div class="right p-5">
+          <div class="title fw-bold fs-1">
+            learn more about our <span class="ms-green">missions</span>
+          </div>
+
+          <div class="divider my-5"></div>
+
+          <div class="text-secondary mb-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem sit asperiores praesentium fugiat molestias inventore quibusdam laboriosam deleniti maiores quod ad explicabo magnam cumque, id tempore laudantium, similique exercitationem odit!</div>
+
+          <button class="ms-btn ms-btn-light-green">Read more</button>
+        </div>
+        <!-- fine parte destra -->
+      </div>
+
+
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name : 'Section2'
+}
+</script>
+
+
+<style lang="scss">
+  .section2{
+    background-color: #f9f9f9;
+  }
+</style>
