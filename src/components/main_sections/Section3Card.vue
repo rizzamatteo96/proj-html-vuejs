18ab30602086a425eb063f5a462c903173841e0e
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-07-20 11:25:46 +0200

    add section3cards animation

--- src/components/main_sections/Section3Card.vue
+++ src/components/main_sections/Section3Card.vue
@@ -3,11 +3,13 @@
     <div class="pointer">
       <img :src="require(`@/assets/img/${cardInfo.img}`)" alt="example" class="rounded rounded-3 w-100">
       <div class="layover rounded rounded-3 d-flex flex-column justify-content-end ps-5 text-white pb-5">
-        <div class="text-capitalize fs-3 fw-bold">
-          {{cardInfo.title}} 
-        </div>
-        <div class="text-uppercase">
-          {{cardInfo.desc}}
+        <div class="text">
+          <div class="text-capitalize fs-3 fw-bold">
+            {{cardInfo.title}} 
+          </div>
+          <div class="text-uppercase">
+            {{cardInfo.desc}}
+          </div>
         </div>
       </div>
     </div>
@@ -37,6 +39,14 @@ export default {
 
     &:hover .layover{
       visibility: visible;
+      opacity: 1;
+
+      .text{
+        animation-name: cardAnimation;
+        animation-duration: 1s;
+        animation-delay: 0.2s;
+        animation-fill-mode: forwards;
+      }
     }
 
     .layover{
@@ -48,6 +58,20 @@ export default {
       height: 100%;
       background-image: linear-gradient(to right, rgba(7, 217, 0, 0.8) , rgba(0, 219, 168, 0.8));
       visibility: hidden;
+      opacity: 0;
+      transition: opacity 1s;
+
+      .text{
+        position: absolute;
+        bottom: 0;
+        left: 10%;
+        opacity: 0;
+      }
     }
+  }
+
+  @keyframes cardAnimation {
+    from{opacity: 0; bottom: 0;}
+    to{opacity: 1; bottom: 10%;}
   }
 </style>
